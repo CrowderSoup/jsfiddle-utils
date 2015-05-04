9291c92d1e80f3d1cf86b7b4edf6cfcfe8de4fd1
Aaron Crowder <aaron@aaroncrowder.com>
2015-05-04 16:47:47 -0600

    Adding some null checking and putting the logger in the right place.

--- jsfiddle-utils.js
+++ jsfiddle-utils.js
@@ -1,12 +1,16 @@
 (function () {
-    var logger = document.getElementById('log');
     
     window.log = function(message){
         console.log(message);
-        if (typeof message == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
-        } else {
-            logger.innerHTML += message + '<br />';
+        
+        // We're only going to log to the body of the page if we have a logger element
+        var logger = document.getElementById('log');
+        if(logger != null){
+            if (typeof message == 'object') {
+                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
+            } else {
+                logger.innerHTML += message + '<br />';
+            }
         }
     };
 })();
